39b38e05c6f8a1e8c8f7add97481edc4b68a86a1
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-07-21 16:29:03 -0700

    2021_07_21
    How to study

---
+++ cert/app/src/main/java/com/example/cert/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.cert
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dDayButton: Button = findViewById(R.id.dDaySetting)
+        val dDayText = findViewById<TextView>(R.id.dDay)
+
+        val certiInfoBtn = findViewById<ImageButton>(R.id.inforBtn)
+        val scheduleBtn: ImageButton = findViewById(R.id.schedule)
+        val channelBtn: ImageButton = findViewById(R.id.youtube)
+        val studyBtn: ImageButton = findViewById(R.id.study)
+
+        //  intent를 통해 넘어온 문자열을 difDate 변수에 담음
+        val difDate = intent.getStringExtra("difDate")
+
+        dDayText.text = difDate
+
+        // D-Day설정 버튼을 눌러 설정창으로 이동
+        dDayButton.setOnClickListener {
+            val intent = Intent(this, DdayActivity::class.java)
+            startActivity(intent)
+        }
+
+        certiInfoBtn.setOnClickListener {
+            val intent = Intent(this,WhatCerti::class.java)
+            startActivity(intent)
+        }
+
+        scheduleBtn.setOnClickListener {
+            val intent = Intent(this,ScheduleCerti::class.java)
+            startActivity(intent)
+        }
+
+        channelBtn.setOnClickListener {
+            val intent = Intent(this,HowToStudy::class.java)
+            startActivity(intent)
+        }
+
+    }
+}

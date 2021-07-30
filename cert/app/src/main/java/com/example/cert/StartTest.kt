85194023b2f4baa5dea8a9c6181d3c9ad2671c37
dltmddyd321 <dltmddyd321@naver.com>
2021-07-30 14:19:48 +0900

    2021_07_30
    미니 테스트 기능 구현 중
    
    1. xml 디자인 제작
    2. data class

---
+++ cert/app/src/main/java/com/example/cert/StartTest.kt
@@ -0,0 +1,11 @@
+package com.example.cert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_test)
+    }
+}
---
+++ cert/app/src/main/java/com/example/cert/Test.kt
@@ -0,0 +1,11 @@
+package com.example.cert
+
+data class Test (
+    val testExp: String,
+    val testImg: String,
+    val test_1: String,
+    val test_2: String,
+    val test_3: String,
+    val test_4: String
+
+)
